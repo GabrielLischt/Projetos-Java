164ae261271c0e63d84845ecd2c82ed7556e7e5c
Gabriel Lischt Saldanha <pet0080409@aluno.firjansenaisesi.com.br>
2025-09-22 20:58:07 -0300

    Aula2

---
+++ CalculadoraMedia.java
@@ -0,0 +1,10 @@
+public class CalculadoraMedia{
+    public static void main(String[] args) {
+        double nota1 =9;
+        double nota2 = 9;
+        double nota3 = 10;
+        double nota4 = 8;
+        double media = (nota1+nota2+nota3+nota4)/4;
+        System.out.println("A média é: " + media);
+    }
+}
---
+++ ExercicioVariaveis.java
@@ -0,0 +1,13 @@
+public class ExercicioVariaveis{
+    public static void main(String[] args) {
+
+        String nome = "João";
+        int idade= 20;
+        double peso = 52.5;
+        double altura = 1.75;
+        System.out.println("O Funcionário " + nome + " tem:");
+        System.out.println("idade: " + idade );
+        System.out.println("altura: " + altura );
+        System.out.println("peso: " + peso);
+    }
+}
